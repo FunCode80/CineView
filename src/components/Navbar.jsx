d88d65b9922f8630e66f4bc17aed116cd0acccd8
FunCode80 <tvsgit@outlook.com>
2026-02-21 14:09:59 +0100

    changed logo and mobile Morviemodal layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import { NavLink } from 'react-router-dom';
+import { Clapperboard } from 'lucide-react'; // Importeer een passend icoon
 
 function Navbar({ favCount }) {
-  return (
-    <nav className="navbar">
-      <NavLink to="/" className="logo"><h1>CineView</h1></NavLink>
-      <div className="nav-links">
-        <NavLink to="/" end>Home</NavLink>
-        <NavLink to="/popular">Populair</NavLink>
-        <NavLink to="/favorites">Favorieten ({favCount})</NavLink>
-      </div>
-    </nav>
-  );
+    
+    return (
+        <nav className="navbar">
+        <NavLink to="/" className="logo">
+            <Clapperboard size={28} color="#e50914" strokeWidth={2.5} />
+            <span>CineView</span>
+        </NavLink>
+        <div className="nav-links">
+            <NavLink to="/" end>Home</NavLink>
+            <NavLink to="/popular">Populair</NavLink>
+            <NavLink to="/favorites">Favorieten ({favCount})</NavLink>
+        </div>
+        </nav>
+    );
 }
 
 export default Navbar;
